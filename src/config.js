b587843ddced15d9f0d309e652c6ebec8072594b
Mihary <miharymandresy12@gmail.com>
2026-02-10 13:06:00 +0300

    feat: part url

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const host = {
+    url: {
+        signalements : "http://localhost:8000/api/signalements",
+        statistique : "http://localhost:8000/api/signalements/statistiques/general",
+        login: "http://localhost:8000/api/login",
+        register: "http://localhost:8000/api/register",
+    }
+}
+
+export {
+    host
+}
